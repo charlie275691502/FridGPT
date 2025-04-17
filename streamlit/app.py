3bab0879a225989340ce3c1d41f7016e146ae237
charlie <charlie275691502@gmail.com>
2025-04-17 22:25:14 +1000

    Fix Absolute path

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from PIL import Image
+import os
 from ultralytics import YOLO
 
-@st.cache_resource
 def load_model():
-    return YOLO(".\\best.pt")  # Make sure best.pt is in the same folder
+    model_path = os.path.join(os.path.dirname(__file__), "best.pt")  # Use Unix-style slashes
+    print(f"Loading model from: {model_path}")  # Debugging line to check path
+    return YOLO(model_path)
 
 model = load_model()
 
